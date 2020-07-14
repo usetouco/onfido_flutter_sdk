9e8c15aa852324e9e5961a02edd14c7ba94c6684
Luca <luca@usetouco.com>
2020-07-14 18:05:15 +0100

    [iOS] Start implementing plugin

--- ios/Classes/SwiftOnfidoFlutterSdkPlugin.swift
+++ ios/Classes/SwiftOnfidoFlutterSdkPlugin.swift
@@ -1,14 +1,47 @@
 import Flutter
 import UIKit
+//import Onfido
 
 public class SwiftOnfidoFlutterSdkPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "onfido_flutter_sdk", binaryMessenger: registrar.messenger())
     let instance = SwiftOnfidoFlutterSdkPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    registrar.addApplicationDelegate(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    let arguments: Dictionary<String, Any> = call.arguments as! Dictionary
+    
+    if (call.method == "startFlow") {
+        let sdkToken: String = arguments["sdkToken"] as! String
+        let steps: Array<String> = arguments["flowSteps"] as! Array<String>
+        
+        startFlow(sdkToken: sdkToken, steps: steps)
+    } else {
+        result(FlutterMethodNotImplemented)
+    }
   }
+    
+    private func startFlow(sdkToken: String, steps: Array<String>) {
+//        let configBuilder = OnfidoConfig.builder()
+//            .withSDKToken("YOUR_SDK_TOKEN_HERE")
+//
+//        for step in steps {
+//            switch step {
+//            case "welcome":
+//                configBuilder.withWelcomeStep()
+//            case "captureDocument":
+//                configBuilder.withDocumentStep()
+//            case "captureFace":
+//                configBuilder.withFaceStep(ofVariant: .photo(withConfiguration: nil))
+//            case "finalScreen":
+//                configBuilder...
+//            default:
+//                // do nothing
+//            }
+//        }
+//
+//        let config = configBuilder.build()
+    }
 }
